8cd0aa9a4ecdaa8ea457dd352e4332a1b7953812
Leonel Valencia <valenciaestudillo@gmail.com>
2023-01-26 15:19:46 -0600

    feat: state todoSearch

--- src/Components/TodoSearch.js
+++ src/Components/TodoSearch.js
@@ -2,16 +2,22 @@ import React from 'react';
 import '../css/TodoSearch.css';
 
 const TodoSearch = () => {
+    const [searchValue, setSearchValue] = React.useState('')
     const onSearchValueChange = (e)=> {
         console.log(e.target.value);
+        setSearchValue(e.target.value)
     }
 
-    return (  
+    return [ 
         <input 
-            className="TodoSearch" placeholder="Cebolla" 
+            className="TodoSearch" 
+            placeholder="Cebolla" 
+            value={searchValue}
             onChange={onSearchValueChange}    
-        />
-    );
+        />,
+        <p>{searchValue}</p>
+    ];
+
 }
  
 export { TodoSearch };
