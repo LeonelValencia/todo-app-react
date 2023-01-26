0edfafd0d5ff9d0608abdac7918ebd97826ac035
Leonel Valencia <valenciaestudillo@gmail.com>
2023-01-26 14:57:14 -0600

    feat: manejo de eventos

--- src/Components/TodoSearch.js
+++ src/Components/TodoSearch.js
@@ -2,8 +2,15 @@ import React from 'react';
 import '../css/TodoSearch.css';
 
 const TodoSearch = () => {
+    const onSearchValueChange = (e)=> {
+        console.log(e.target.value);
+    }
+
     return (  
-        <input className="TodoSearch" placeholder="Cebolla" />
+        <input 
+            className="TodoSearch" placeholder="Cebolla" 
+            onChange={onSearchValueChange}    
+        />
     );
 }
  
