d804f43433491f8219ca9a65dc9fa2a62d8a3779
Leonel Valencia <valenciaestudillo@gmail.com>
2023-01-26 17:31:02 -0600

    feat: contando y buscando TODOs

--- src/App.js
+++ src/App.js
@@ -5,22 +5,45 @@ import { CreateTodoButton } from "./Components/CreateTodoButton";
 import { TodoList } from "./Components/TodoList";
 import { TodoItem } from "./Components/TodoItem";
 import './App.css';
-const todos = [
+const defaultTodos = [
   { text: 'cortar cebolla', completed: true},
   { text: 'Tomar el curso de intro a react', completed: false},
-  { text: 'llorar con la llorona', completed: false},
+  { text: 'llorar con la llorona', completed: true},
 ]
+
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+  const completedTodos = todos.filter(todo => todo.completed).length;
+  const totalTodos = todos.length;
+  let searchedTodos = [];
+
+  if (!searchValue.length >= 1){
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase()
+      const searchText = searchValue.toLowerCase()
+      return todoText.includes(searchText);
+    })
+  }
+
   return (
     <React.Fragment>
       <h1>TODO APP</h1>
       <div className='app'>
-        <TodoCounter />
+        <TodoCounter 
+          total={totalTodos}
+          completed={completedTodos}
+        />
 
-        <TodoSearch />
+        <TodoSearch 
+          searchValue={searchValue} 
+          setSearchValue={setSearchValue}
+        />
         
         <TodoList>
-          {todos.map(todo => (
+          {searchedTodos.map(todo => (
             <TodoItem key={todo.text} text={todo.text} completed={todo.completed}/>
           ))}
         </TodoList>
--- src/Components/TodoSearch.js
+++ src/Components/TodoSearch.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import '../css/TodoSearch.css';
 
-const TodoSearch = () => {
-    const [searchValue, setSearchValue] = React.useState('')
+const TodoSearch = ({searchValue, setSearchValue}) => {
     const onSearchValueChange = (e)=> {
         console.log(e.target.value);
         setSearchValue(e.target.value)
     }
 
-    return [ 
+    return (
         <input 
             className="TodoSearch" 
             placeholder="Cebolla" 
             value={searchValue}
             onChange={onSearchValueChange}    
-        />,
-        <p>{searchValue}</p>
-    ];
+        />
+    );
 
 }
  
